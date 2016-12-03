1fb71b1e3d4f94eff8bf9e48b3ab184a58d97648
JamesLindstrom <jimmyaeolus@gmail.com>
2016-12-03 18:39:58 -0500

    Remove old tests and create new test 1

--- kata.js
+++ kata.js
@@ -1,13 +1,7 @@
 //Main object
 var main = {
 	arabicToRoman : function(arabic){
-		var roman = "";
-		if(arabic >= 5){
-			roman += "V";
-			arabic -= 5;
-		}
-		roman += "I".repeat(arabic);
-		return roman;
+		return "M";
 	}
 };
 
@@ -15,16 +9,7 @@ var main = {
 var test = {
 	run : function(){
 		//Test 1
-		test.compare(1, main.arabicToRoman(1), "I");
-		
-		//Test 2
-		test.compare(2, main.arabicToRoman(2), "II");
-		
-		//Test 3
-		test.compare(3, main.arabicToRoman(5), "V");
-		
-		//Test 4
-		test.compare(4, main.arabicToRoman(6), "VI");
+		test.compare(1, main.arabicToRoman(1000), "M");
 	},
 	
 	//Does a comparison between an input and an output.
