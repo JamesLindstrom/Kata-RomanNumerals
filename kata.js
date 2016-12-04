5e425e56dbaa743de80e619b28902ceba7b56874
JamesLindstrom <jimmyaeolus@gmail.com>
2016-12-03 19:34:53 -0500

    Test 8 and modify old tests
    The old tests were designed to show that the correct number of letters would be returned even if the number was slightly bigger than it had to be. Now this causes issues because the smaller numbers are represented by letters as well. This was also a large step because the logic that makes 1000s, 900s, 500s, and 400s work is the same as the logic that makes 100s, 90s, 50s, and 40s work.

--- kata.js
+++ kata.js
@@ -2,6 +2,8 @@
 var main = {
 	arabicToRoman : function(arabic){
 		var roman = "";
+		
+		//1000s and 500s
 		var m = Math.floor(arabic / 1000);
 		roman += "M".repeat(m);
 		arabic -= m * 1000;
@@ -15,6 +17,22 @@ var main = {
 			roman += "CD";
 			arabic -= 400;
 		};
+		
+		//100s and 50s
+		var c = Math.floor(arabic / 100);
+		roman += "C".repeat(c);
+		arabic -= c * 100;
+		if(arabic >= 90){
+			roman += "XC";
+			arabic -= 90;
+		}else if(arabic >= 50){
+			roman += "L";
+			arabic -= 50;
+		}else if(arabic >= 40){
+			roman += "XL";
+			arabic -= 40;
+		};
+		
 		return roman;
 	}
 };
@@ -26,7 +44,7 @@ var test = {
 		test.compare(1, main.arabicToRoman(1000), "M");
 		
 		//Test 2
-		test.compare(2, main.arabicToRoman(2060), "MM");
+		test.compare(2, main.arabicToRoman(2000), "MM");
 		
 		//Test 3
 		test.compare(3, main.arabicToRoman(900), "CM");
@@ -35,13 +53,16 @@ var test = {
 		test.compare(4, main.arabicToRoman(1900), "MCM");
 		
 		//Test 5
-		test.compare(5, main.arabicToRoman(2950), "MMCM");
+		test.compare(5, main.arabicToRoman(2900), "MMCM");
 		
 		//Test 6
-		test.compare(6, main.arabicToRoman(2550), "MMD");
+		test.compare(6, main.arabicToRoman(2500), "MMD");
 		
 		//Test 7
-		test.compare(7, main.arabicToRoman(3450), "MMMCD");
+		test.compare(7, main.arabicToRoman(3400), "MMMCD");
+		
+		//Test 8
+		test.compare(8, main.arabicToRoman(2340), "MMCCCXL");
 	},
 	
 	//Does a comparison between an input and an output.
